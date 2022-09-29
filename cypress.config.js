59be140092d1aa283d5ec2a2d056288a9fa2d043
Pierre Swtich <a.stolarski.as@gmail.com>
2022-09-29 11:37:51 +0200

    Completing my first test with cypress & Updating the config File: * Timeout * Do not check for code changes

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,14 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      config.defaultCommandTimeout = 5000
+
+      config.watchForFileChanges = false
+
+
+      return config
+      
     },
   },
+
 });
